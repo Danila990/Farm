9655386e0f533a86f0e4efc529d530ea84179d0f
Данила <106236167+Danila990@users.noreply.github.com>
2025-07-05 22:39:13 +0300

    patch 0.0.0.64

--- Assets/_Project/Scripts/Player/MoveUnit.cs
+++ Assets/_Project/Scripts/Player/MoveUnit.cs
@@ -1,34 +1,28 @@
 using System.Threading;
 using System.Threading.Tasks;
-using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 
 namespace MyCode
 {
     public class MoveUnit : MonoBehaviour
     {
-        [SerializeField] private float _moveSpeed = 3f;
-        [SerializeField] private Vector3 _moveOffset;
-
-        [Tooltip("Высота прыжка")]
-        public float jumpHeight = 2f;
-
-        [Tooltip("Скорость движения")]
-        public float jumpDuration = 1f;
+        [SerializeField] private float _jumpHeight = 2f;
+        [SerializeField] private float _jumpDuration = 1f;
+        [SerializeField] private Vector3 _offset;
 
         public async Task JumpToAsync(Vector3 targetPosition, CancellationToken token = default)
         {
-            targetPosition += _moveOffset;
+            targetPosition += _offset;
             Vector3 startPosition = transform.position;
             float elapsed = 0f;
 
-            while (elapsed < jumpDuration)
+            while (elapsed < _jumpDuration)
             {
                 elapsed += Time.deltaTime;
-                float t = Mathf.Clamp01(elapsed / jumpDuration);
+                float t = Mathf.Clamp01(elapsed / _jumpDuration);
 
                 Vector3 horizontalPos = Vector3.Lerp(startPosition, targetPosition, t);
-                float heightOffset = 4 * jumpHeight * t * (1 - t);
+                float heightOffset = 4 * _jumpHeight * t * (1 - t);
                 transform.position = new Vector3(horizontalPos.x, horizontalPos.y + heightOffset, horizontalPos.z);
 
                 await Task.Yield();
