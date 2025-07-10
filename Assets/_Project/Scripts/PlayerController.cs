dd2cbce4e3ecc6ff83acadbacd8f0be9c4cd9a57
Данила <106236167+Danila990@users.noreply.github.com>
2025-07-10 21:42:10 +0300

    patch 0.0.0.711

--- Assets/_Project/Scripts/PlayerController.cs
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -10,6 +10,16 @@ namespace MyCode
 
         private IGridMap _gridMap;
 
+        private void OnEnable()
+        {
+            EventBus.Subscribe(EventType.Dead, SetPlayerSpawnPos);
+        }
+
+        private void OnDisable()
+        {
+            EventBus.Unsubscribe(EventType.Dead, SetPlayerSpawnPos);
+        }
+
         public void SetupMap(IGridMap gridMap)
         {
             _gridMap = gridMap;
@@ -17,6 +27,7 @@ namespace MyCode
 
         public void SetPlayerSpawnPos()
         {
+            Player.Setup(_gridMap, _gridMap.FindPlatform(PlatformType.PlayerSpawn).GridIndex, _settings);
             Vector3 spawnPos = _gridMap.FindPlatform(PlatformType.PlayerSpawn).transform.position;
             spawnPos.y += _settings.OffsetY;
             Player.transform.position = spawnPos;
@@ -26,7 +37,6 @@ namespace MyCode
         {
             Player = Instantiate(_settings.PlayerPrefab);
             SetPlayerSpawnPos();
-            Player.Setup(_gridMap, _gridMap.FindPlatform(PlatformType.PlayerSpawn).GridIndex, _settings);
         }
     }
 }
---
+++ Assets/_Project/Scripts/UI/ScoreView.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+
+namespace MyCode
+{
+    public class ScoreView : MonoBehaviour
+    {
+        [SerializeField] TMP_Text _text;
+
+        private int _score;
+
+        private void OnEnable()
+        {
+            EventBus.Subscribe(EventType.Coin, UpdateView);
+            EventBus.Subscribe(EventType.Dead, ResetView);
+        }
+
+        private void OnDisable()
+        {
+            EventBus.Unsubscribe(EventType.Coin, UpdateView);
+            EventBus.Unsubscribe(EventType.Dead, ResetView);
+        }
+
+        private void UpdateView()
+        {
+            _score++;
+            _text.text = _score.ToString();
+        }
+        private void ResetView()
+        {
+            _score = 0;
+            _text.text = _score.ToString();
+        }
+    }
+}
